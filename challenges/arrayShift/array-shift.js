03045cbd33497f3803588b5993845e745a3e7d77
Tess Lameyer <tjlameyer@gmail.com>
2019-12-04 12:59:53 -0800

    worked hard to get the test passing but could not quite get solution

--- challenges/arrayShift/array-shift.js
+++ challenges/arrayShift/array-shift.js
@@ -1,11 +1,17 @@
 const insertShiftArray = (array, value) => {
   const middleIndex = Math.ceil((array.length - 1) / 2); 
   let result = [];
-  let resultIndex = 0; 
+  let i; 
   
-  for(let i = 0; i < array.length; i++) {
-    i === middleIndex ? result[resultIndex] = value : result[resultIndex] = array[i]; 
-    resultIndex++;
+  //first part of array up until the middle
+  for(let i = 0; i < middleIndex; i++) {
+    result[i] = array[i]; 
+  }
+  //inserting into the middle
+  result[middleIndex] = value;
+  //rest of the array 
+  for(middleIndex + 1; i > middleIndex && i < array.length; i++) {
+    result[i] = array[i];
   }
   return result; 
 };
