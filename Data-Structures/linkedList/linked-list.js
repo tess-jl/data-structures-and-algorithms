0bd547aa8f746bf5001dc5162acaad4d14d68dd2
Tess Lameyer <tjlameyer@gmail.com>
2019-12-09 16:29:16 -0800

    linked list code from whiteboarding

--- Data-Structures/linkedList/linked-list.js
+++ Data-Structures/linkedList/linked-list.js
@@ -1,32 +1,46 @@
-// Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
-class NodeLinkedList {
-  constructor{
-    this.nodeValue = nodeValue;
-    this.nextNode = nextNode; 
-
-    // Within your LinkedList class, include a head property. Upon instantiation, an empty Linked List should be created.
-    this.head = head;
+//code shared with me and Joel from our whiteboarding exercise
+class Node {
+  constructor(value) {
+    this.value = value;
   }
-
-  insert (anyValue) {
-    // Define a method called insert which takes any value as an argument and adds a new node with that value to the head of the list with an O(1) Time performance.
-
-  }
-
-  includes (anyValue) {
-    // Define a method called includes which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
-
-  }
-
-  toString () {
-    // Define a method called toString which takes in no arguments and returns a string representing all the values in the Linked List.
-
-  }
-
-
 }
+class LinkedList {
+  constructor(value) {
+    this.head = null;
+    this.insert(value);
+  }
 
+  insert(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    return this;
+  }
 
-// Any exceptions or errors that come from your code should be semantic, capturable errors. For example, rather than a default error thrown by your language, your code should raise/throw a custom, semantic error that describes what went wrong in calling the methods you wrote for this lab.
+  includes(value) {
+    let thisNode = this.head;
+    while(thisNode){
+      if(thisNode.value === value) return true;
+      thisNode = thisNode.next;
+    }
+    return false;
+  }
 
-// Be sure to follow your language/frameworks standard naming conventions (e.g. C# uses PascalCasing for all method and class names).
+  toString() {
+    let thisNode = this.head;
+    let valuesStringed = '';
+    while(thisNode){
+      if(!thisNode.next) {
+        valuesStringed += thisNode.value;
+      } else {
+        valuesStringed += thisNode.value + '-> ';
+      }
+      thisNode = thisNode.next;
+    }
+    return valuesStringed;
+  }
+}
+module.exports = {
+  Node,
+  LinkedList
+};
