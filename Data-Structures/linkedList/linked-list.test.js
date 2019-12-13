037f6a3fa84216242d39f2f3ae800c45a431f852
Tess Lameyer <tjlameyer@gmail.com>
2019-12-12 16:23:10 -0800

    tests written for includes method on linked list class

--- Data-Structures/linkedList/linked-list.test.js
+++ Data-Structures/linkedList/linked-list.test.js
@@ -1,14 +1,31 @@
 const { LinkedList } = require('./linked-list'); 
 
 describe('linked list tests', () => {
+  let ll;
+  beforeEach(() => {
+    ll = new LinkedList();
+  });
+
+  it('instantiates an empty linked list', () => {
+    expect(ll.head).toBeNull();
+  });
 
   it('inserts a new node with the given value', () => {
-    let ll = new LinkedList();
     ll.insert(1);
     ll.insert(2);
 
     expect(ll.head.value).toEqual(2);
     expect(ll.toString()).toEqual('2 -> 1');
+  });
+
+  it('tests if a specific value is included in the linked list', () => {
+    ll.insert(1);
+    ll.insert(2);
+    const test1 = ll.includes(1);
+    const test2 = ll.includes(3);
+
+    expect(test1).toEqual(true);
+    expect(test2).toEqual(false);
   });
   
   // it('adds a new node with the given value to the end of the list', () => {
