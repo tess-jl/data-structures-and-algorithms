9a6ac37af94d88b5d72471c58d3f54447b04b515
Tess Lameyer <tjlameyer@gmail.com>
2019-12-18 17:36:11 -0800

    separated the tests for true and false to be more clear

--- challenges/multiBracketValidation/multi-bracket-validation.test.js
+++ challenges/multiBracketValidation/multi-bracket-validation.test.js
@@ -1,13 +1,16 @@
 const { multiBracketValidation } = require('./multi-bracket-validation.js');
   
 describe('testing the multi bracket validation function', () => {
-  it('should validate based on brackets in the input string and return a boolean', () => {
+  it('should validate based on brackets in the input string and return true if all brackets are accounted for', () => {
 
-    // expect(multiBracketValidation('{}')).toEqual(true);
-    // expect(multiBracketValidation('{}(){}')).toEqual(true);
+    expect(multiBracketValidation('{}')).toEqual(true);
+    expect(multiBracketValidation('{}(){}')).toEqual(true); 
     expect(multiBracketValidation('()[[Extra Characters]]')).toEqual(true);
     expect(multiBracketValidation('(){}[[]]')).toEqual(true);
     expect(multiBracketValidation('{}{Code}[Fellows](())')).toEqual(true);
+  });
+
+  it('should validate based on brackets in the input string and return true if all brackets are accounted for', () => {
 
     expect(multiBracketValidation('[({}]')).toEqual(false);
     expect(multiBracketValidation('(](')).toEqual(false);
@@ -15,6 +18,5 @@ describe('testing the multi bracket validation function', () => {
     expect(multiBracketValidation('{')).toEqual(false);
     expect(multiBracketValidation(')')).toEqual(false);
     expect(multiBracketValidation('[}')).toEqual(false);
-
   });
 }); 
