3a806eda4c5d55f0d6dbf3854af8d8d5df5ee495
Tess Lameyer <tjlameyer@gmail.com>
2020-01-28 23:10:24 -0800

    config for tree code challenge

---
+++ Data-Structures/tree/tree.js
@@ -0,0 +1,28 @@
+class Node {
+  constructor(value, left, right) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BinaryTree {
+
+  preOrder() {
+
+  }
+
+  // inOrder()
+  // postOrder()
+  //custom errors
+}
+
+// class SearchBinaryTree {
+
+//   add() {
+
+//   }
+
+//   // contains()
+
+// }
